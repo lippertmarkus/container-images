4c028cac6ab44dd7ffdf73e49b668d11b05b1a0c
Markus Lippert <lippertmarkus@gmx.de>
2021-12-13 15:20:36 +0100

    fix update check

--- traefik/update.sh
+++ traefik/update.sh
@@ -5,7 +5,7 @@
 echo "checking updates for traefik!"
 latest=$(curl -sL "https://api.github.com/repos/traefik/traefik/releases/latest" | jq -r ".tag_name")
 
-if [ -z "$latest" ] && [ "$latest" != "null" ] && [[ "$version" != "$latest" ]]; then
+if [ ! -z "$latest" ] && [ "$latest" != "null" ] && [[ "$version" != "$latest" ]]; then
     echo "New version available!"
     echo $latest > version
 fi
